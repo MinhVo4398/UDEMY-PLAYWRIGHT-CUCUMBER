c5e44ac9277e8eef20c5727b3fce20a07f12bd76
Minh Vo <minh.vo@noveogroup.com>
2024-02-07 17:02:11 +0700

    cucumber report

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -1,16 +1,27 @@
 const { Given, When, Then, defineStep } = require('@cucumber/cucumber')
-const {LoginPage} = require ('../page-objects/login-page');
+const { LoginPage } = require('../page-objects/login-page')
 
-const loginPage = new LoginPage();
+const loginPage = new LoginPage()
 
-defineStep("I visit a login page", async function() {
-   await loginPage.navigateToLoginScreen();
+defineStep('I visit a login page', async function () {
+  await loginPage.navigateToLoginScreen()
 })
 
-defineStep("I fill the login form with valid credentials", async function () {
-   await loginPage.submitLoginForm();
+defineStep('I fill the login form with valid credentials', async function () {
+  await loginPage.submitLoginForm()
 })
 
-defineStep("I should see the home page", async function() {
+defineStep('I should see the home page', async function () {
   await loginPage.assertUserIsLoggedIn
 })
+
+defineStep('I wait for 3 seconds', async function () {
+  await loginPage.pause()
+})
+
+defineStep(
+  /^I fill the login form with "([^"]*)" and "([^"]*)"$/,
+  async function (username, password) {
+    await loginPage.submitLoginWithParameters(username, password)
+  },
+)
